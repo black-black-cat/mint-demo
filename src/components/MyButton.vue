9d00bc0d193dabc09eb2a85c943d1e7576b61ea4
black-black-cat <k99apply@gmail.com>
2017-12-03 00:11:12 +0800

    Add route "button"

---
+++ src/components/MyButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <mt-button>button</mt-button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import MyButtom from '@/components/MyButtom'
+import MyButton from '@/components/MyButton'
 
 Vue.use(Router)
 
@@ -13,8 +13,8 @@ export default new Router({
       component: HelloWorld
     }, {
       path: '/button',
-      name: 'button'
-      component: MyButtom
+      name: 'button',
+      component: MyButton
     }
   ]
 })
